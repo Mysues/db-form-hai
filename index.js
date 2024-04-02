de9068c6657952ca9dbe4ef39775e670fe068264
uthman1 <uthman@e-shore.com.my>
2024-04-02 21:18:27 +0800

    add car_ prefix to manufacturer and model use query instead of param

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 import express from 'express'
-import {getCarManufacturerAll} from './database.js'
+import {getCarManufacturerAll,getCarModel} from './database.js'
 
 const app = express();
 
-app.get("/manufacturer",async (req,res)=>{
-    const data = await getCarManufacturerAll()
+app.get("/car_manufacturer",async (req,res)=>{
+    const manufacturer = await getCarManufacturerAll()
     
-    res.send(data.map(car => car.manufacturer));
+    res.send(manufacturer.map(car => car.manufacturer));
+})
+
+app.get("/car_model",async (req,res)=>{
+    const manufacturer = req.query.manufacturer;
+    const model = await getCarModel(manufacturer)
+    
+    res.send(model.map(car => car.model));
 })
 
 app.listen(8080,()=>{
