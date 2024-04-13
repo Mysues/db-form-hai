8f27030b6ae2e829d745f7f18e2152130493f478
syed93uthman <syed_uthman@yahoo.com>
2024-04-13 10:21:30 +0800

    simplified import database.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import express from 'express'
-import {getCarManufacturerAll,getCarModel,getCarRegisterNumber,getCarID,createUserProfile} from './database.js'
+import * as db from './database.js'
 import bodyParser from 'body-parser';
+
+const {
+    getCarManufacturerAll,
+    getCarModel,
+    getCarRegisterNumber,
+    getCarID,
+    createUserProfile
+} = db;
 
 const app = express();
 
@@ -8,14 +16,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/car_manufacturer",async (req,res)=>{
-    const manufacturer = await getCarManufacturerAll()
+    const manufacturer = await db.getCarManufacturerAll()
     
     res.send(manufacturer.map(car => car.manufacturer));
 })
 
 app.get("/car_model",async (req,res)=>{
     const manufacturer = req.query.manufacturer;
-    const model = await getCarModel(manufacturer)
+    const model = await db.getCarModel(manufacturer)
     
     res.send(model.map(car => car.model));
 })
@@ -23,7 +31,7 @@ app.get("/car_model",async (req,res)=>{
 app.get("/registration_number",async (req,res)=>{
     const car_manufacturer = req.query.car_manufacturer;
     const car_model = req.query.car_model;
-    const registration_number = await getCarRegisterNumber(car_manufacturer,car_model)
+    const registration_number = await db.getCarRegisterNumber(car_manufacturer,car_model)
     
     res.send(registration_number.map(car => car.register_number));
 })
@@ -32,14 +40,14 @@ app.get("/car_id",async (req,res)=>{
     const car_manufacturer = req.query.car_manufacturer;
     const car_model = req.query.car_model;
     const registration_number = req.query.registration_number;
-    const car_id = await getCarID(car_manufacturer,car_model,registration_number)
+    const car_id = await db.getCarID(car_manufacturer,car_model,registration_number)
     
     res.send(car_id[0]);
 })
 
 app.post("/create_user",async (req,res)=>{
     const userProfile = await req.body;
-    createUserProfile(userProfile);
+    db.createUserProfile(userProfile);
     res.send(userProfile);
 })
 
